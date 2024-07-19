cd570b9f3053b9b5318014b071f7abbe77312af0
wentzpatricia <patriciawentzm@gmail.com>
2024-07-19 18:23:16 -0300

    feat: começando a implementar meus voluntariados

--- src/app/pages/volunteer/my-volunteering/my-volunteering.component.ts
+++ src/app/pages/volunteer/my-volunteering/my-volunteering.component.ts
@@ -1,8 +1,32 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { EventTypeEnum } from '../../../core/_utils/EventType.enum';
 
 @Component({ selector: 'app-my-volunteering', templateUrl: './my-volunteering.component.html', styleUrl: './my-volunteering.component.scss' })
-export class MyVolunteeringComponent implements OnInit{
+export class MyVolunteeringComponent {
 
-    ngOnInit() { }
+    EventTypeEnum = EventTypeEnum; 
+
+    date = [
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.BATH
+        },
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.EVENT
+        },
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.TRANSPORT
+        },
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.BATH
+        }
+    ]
 }
 
