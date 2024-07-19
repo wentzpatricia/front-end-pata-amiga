ae62ab9ee5691f629be9bae99dc96ed28586dc21
wentzpatricia <patriciawentzm@gmail.com>
2024-07-19 16:23:37 -0300

    feat: adicionando botao dropdown

--- src/app/pages/volunteer/volunteer.module.ts
+++ src/app/pages/volunteer/volunteer.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { EventsComponent } from './events/events.component';
 import { MyVolunteeringComponent } from './my-volunteering/my-volunteering.component';
 
@@ -8,7 +10,15 @@ import { VolunteerRoutingModule } from './volunteer-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [EventsComponent, MyVolunteeringComponent],
-  imports: [CommonModule, SharedModule, VolunteerRoutingModule],
+  declarations: [
+    EventsComponent, 
+    MyVolunteeringComponent
+  ],
+  imports: [
+    CommonModule,
+    NgbDropdownModule, 
+    SharedModule, 
+    VolunteerRoutingModule
+  ],
 })
 export class VolunteerModule {}
