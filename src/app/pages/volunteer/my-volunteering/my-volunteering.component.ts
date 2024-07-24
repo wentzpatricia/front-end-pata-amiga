68bcc373eb8e333f67bb59c0e991efe2f848d66e
Patrícia <85044012+wentzpatricia@users.noreply.github.com>
2024-07-24 15:19:06 -0300

    Feature/meus voluntariados (#6)
    * feat: começando a implementar meus voluntariados
    
    * feature: adicionando estilo para meus voluntariados
    
    * feat: adicionando responsividade para meus voluntariados

--- src/app/pages/volunteer/my-volunteering/my-volunteering.component.ts
+++ src/app/pages/volunteer/my-volunteering/my-volunteering.component.ts
@@ -1,21 +1,49 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { EventDataService } from '../../ong/_services/eventData.service';
 import { Auth } from '@angular/fire/auth';
 import { EventInterface } from '../../ong/_models/event.interface';
+import { EventTypeEnum } from '../../../core/_utils/EventType.enum';
+
 @Component({ selector: 'app-my-volunteering', templateUrl: './my-volunteering.component.html', styleUrl: './my-volunteering.component.scss' })
 
 export class MyVolunteeringComponent implements OnInit {
     constructor( private firebaseAuth: Auth, private firestore: Firestore, private eventDataService: EventDataService) {}
-    events: EventInterface[] | any = [];
+    EventTypeEnum = EventTypeEnum; 
+    events: EventInterface[] = [
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.BATH
+        },
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.EVENT
+        },
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.TRANSPORT
+        },
+        {
+            date_at: '25 de julho às 18h',
+            local: 'Avenida João Wallig, 1800,Jardim Europa, Porto Alegre - RS',
+            type: EventTypeEnum.BATH
+        }
+    ];
 
     ngOnInit(): void {
-        if (this.firebaseAuth.currentUser !== null) {
-            if (this.firebaseAuth.currentUser.uid) {
-                this.eventDataService.getByUser(this.firebaseAuth.currentUser.uid).then((data) => {
-                    this.events = data
-                })
-            }
-        }
+        // if (this.firebaseAuth.currentUser !== null) {
+        //     if (this.firebaseAuth.currentUser.uid) {
+        //         this.eventDataService.getByUser(this.firebaseAuth.currentUser.uid).then((data) => {
+        //             if(data)
+        //             this.events = data;
+        //         })
+        //     }
+        // }
     }
+
+    
 }
+
